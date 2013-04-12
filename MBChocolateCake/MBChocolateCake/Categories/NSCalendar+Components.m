c27ce2e45ac866606372430a4a88dc5687aa9c39
Moshe Berman <moshberm@gmail.com>
2013-04-12 18:59:37 -0400

    Added a convenience category to NSCalendar which returns specific component units from a date.

---
+++ MBChocolateCake/MBChocolateCake/Categories/NSCalendar+Components.m
@@ -0,0 +1,57 @@
+//
+//  NSCalendar+Components.m
+//  MBChocolateCake
+//
+//  Created by Moshe Berman on 4/12/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "NSCalendar+Components.h"
+
+@implementation NSCalendar (Components)
+
+- (NSInteger)secondsInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSSecondCalendarUnit fromDate:date];
+    return [comps second];
+}
+
+- (NSInteger)minutesInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSMinuteCalendarUnit fromDate:date];
+    return [comps minute];
+}
+
+- (NSInteger)hoursInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSHourCalendarUnit fromDate:date];
+    return [comps hour];
+}
+
+- (NSInteger)daysInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSDayCalendarUnit fromDate:date];
+    return [comps day];
+}
+
+- (NSInteger)monthsInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSMonthCalendarUnit fromDate:date];
+    return [comps month];
+}
+
+- (NSInteger)yearsInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSYearCalendarUnit fromDate:date];
+    return [comps year];
+}
+
+- (NSInteger)eraInDate:(NSDate*)date
+{
+    NSDateComponents *comps = [self components:NSEraCalendarUnit fromDate:date];
+    return [comps era];
+}
+
+
+
+@end
