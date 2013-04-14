6ba6af13683e5a04d2b79b0d63840d7395001bda
Moshe Berman <moshberm@gmail.com>
2013-04-14 04:08:08 -0400

    Added a header view to the project.

---
+++ MBChocolateCake/MBChocolateCake/Core/CKCakeHeaderView.m
@@ -0,0 +1,141 @@
+//
+//  CKCakeHeaderView.m
+//  MBChocolateCake
+//
+//  Created by Moshe Berman on 4/14/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "CKCakeHeaderView.h"
+
+@interface CKCakeHeaderView ()
+{
+    NSUInteger _columnCount;
+}
+
+@property (nonatomic, strong) UILabel *monthTitle;
+@property (nonatomic, strong) NSMutableArray *columnTitles;
+@property (nonatomic, strong) NSMutableArray *columnLabels;
+@property (nonatomic, strong) UIButton *forwardButton;
+@property (nonatomic, strong) UIButton *backwardButton;
+
+@end
+
+@implementation CKCakeHeaderView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+        _monthTitle = [UILabel new];
+        [_monthTitle setTextColor:[UIColor darkGrayColor]];
+        [_monthTitle setShadowOffset:CGSizeMake(0, 1)];
+        [_monthTitle setBackgroundColor:[UIColor clearColor]];
+        [_monthTitle setTextAlignment:NSTextAlignmentCenter];
+        
+        _columnTitles = [NSMutableArray new];
+        _columnLabels = [NSMutableArray new];
+
+        _forwardButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        _backwardButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    }
+    return self;
+}
+
+
+- (void)willMoveToSuperview:(UIView *)newSuperview
+{
+    [self reload];
+    [self layoutSubviews];
+}
+
+- (void)layoutSubviews
+{
+    
+    /* Remove old labels if they exist */
+    for (UILabel *l in [self columnLabels]) {
+        [l removeFromSuperview];
+    }
+    
+    [[self columnLabels] removeAllObjects];
+    
+    /* Arrange the new labels */
+    
+    CGFloat labelWidth = [self frame].size.width/_columnCount;
+    CGFloat labelHeight = 15;
+    
+    for (NSUInteger i = 0; i < [[self columnLabels] count]; i++) {
+        NSString *title = [self columnTitles][i];
+
+        UILabel *label = [self _columnLabelWithTitle:title];
+        [[self columnLabels] addObject:label];
+        
+        CGRect frame = CGRectMake(i*labelWidth, [self frame].size.height-labelHeight, labelWidth, labelHeight);
+        [label setFrame:frame];
+        
+        [self addSubview:label];
+    }
+    
+    /* Show the title Label */
+    CGRect frame = CGRectMake(0, 0, [self frame].size.width, 29);
+    [[self monthTitle] setFrame:frame];
+    
+    /* Show the forward and back buttons */
+    
+}
+
+#pragma mark - Convenience Methods
+
+- (UILabel *)_columnLabelWithTitle:(NSString *)title
+{
+    UILabel *l = [UILabel new];
+    [l setBackgroundColor:[UIColor clearColor]];
+    [l setTextColor:[UIColor darkGrayColor]];
+    [l setFont:[UIFont systemFontOfSize:10]];
+    [l setShadowColor:[UIColor whiteColor]];
+    [l setShadowOffset:CGSizeMake(0, 0.5)];
+    [l setText:title];
+    return l;
+}
+
+- (void)reload
+{
+    /*  STEP 1: Require a data source for the header to be installed */
+    if (![self dataSource]) {
+        @throw [NSException exceptionWithName:@"CKCakeViewHeaderException" reason:@"Header can't be installed without a data source" userInfo:@{@"Header": self}];
+    }
+    
+    /* STEP 2: Now query the data source for the number of columns. */
+    _columnCount = [[self dataSource] numberOfColumnsForHeader:self];
+    
+    /* STEP 3:  Query the datasource for the titles.*/
+    [[self columnTitles] removeAllObjects];
+    
+    for (NSUInteger column = 0; column < _columnCount; column++) {
+        NSString *title = [[self dataSource] titleForColumnAtIndex:column inHeader:self];
+        [[self columnTitles] addObject:title];
+    }
+    
+    /* STEP 4: Month Name */
+    NSString *title = [[self dataSource] titleForHeader:self];
+    [[self monthTitle] setText:title];
+}
+
+#pragma mark - Button Handling
+
+- (void)forwardButtonTapped
+{
+    if ([[self delegate] respondsToSelector:@selector(forwardTapped)]) {
+        [[self delegate] forwardTapped];
+    }
+}
+
+- (void)backwardButtonTapped
+{
+    if ([[self delegate] respondsToSelector:@selector(backwardTapped)]) {
+        [[self delegate] backwardTapped];
+    }
+}
+
+@end
