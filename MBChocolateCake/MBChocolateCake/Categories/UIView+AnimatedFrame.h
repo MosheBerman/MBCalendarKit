bc64dc93105c9cdcd711ec1473b272e8d960dfe1
Moshe Berman <moshberm@gmail.com>
2013-04-15 19:40:04 -0400

    Added a category to the project, exposing an animated way to change the frame of a view.

---
+++ MBChocolateCake/MBChocolateCake/Categories/UIView+AnimatedFrame.h
@@ -0,0 +1,13 @@
+//
+//  UIView+AnimatedFrame.h
+//  MBChocolateCake
+//
+//  Created by Moshe Berman on 4/15/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (AnimatedFrame)
+
+@end
