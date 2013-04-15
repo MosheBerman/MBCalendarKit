62e241c0bfcb4fc7523322705f93912f9516ee26
Moshe Berman <moshberm@gmail.com>
2013-04-14 20:36:00 -0400

    Added a CKCakeEvent class.

---
+++ MBChocolateCake/MBChocolateCake/Core/CKCakeEvent.h
@@ -0,0 +1,17 @@
+//
+//  CKCakeEvent.h
+//  MBChocolateCake
+//
+//  Created by Moshe Berman on 4/14/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKCakeEvent : NSObject
+
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSDictionary *info;
+
+@end
---
+++ MBChocolateCake/MBChocolateCake/Core/CKCakeEvent.m
@@ -0,0 +1,13 @@
+//
+//  CKCakeEvent.m
+//  MBChocolateCake
+//
+//  Created by Moshe Berman on 4/14/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "CKCakeEvent.h"
+
+@implementation CKCakeEvent
+
+@end
