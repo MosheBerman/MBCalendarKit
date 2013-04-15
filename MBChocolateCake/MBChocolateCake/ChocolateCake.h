4d202c9d8069345e084739b15e505563dee1483d
Moshe Berman <moshberm@gmail.com>
2013-04-15 01:30:15 -0400

    Added ChocolateCake.h to project.

---
+++ MBChocolateCake/MBChocolateCake/ChocolateCake.h
@@ -0,0 +1,15 @@
+//
+//  ChocolateCake.h
+//  MBChocolateCake
+//
+//  Created by Moshe Berman on 4/15/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef MBChocolateCake_ChocolateCake_h
+#define MBChocolateCake_ChocolateCake_h
+
+#import "CKCakeEvent.h"
+#import "CKCakeViewController.h"
+
+#endif
