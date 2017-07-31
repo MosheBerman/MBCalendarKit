c645af2eeba29864b33920549af94897b4d46a36
Moshe Berman <moshefromsinai@gmail.com>
2017-07-31 14:41:00 -0400

    Added a wrapper class to the project to assist with AutoLayout code.

---
+++ MBCalendarKit/CalendarKit/Core/CKCalendarAnimationWrapperView.h
@@ -0,0 +1,13 @@
+//
+//  CKCalendarAnimationWrapperView.h
+//  MBCalendarKit
+//
+//  Created by Moshe Berman on 7/31/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKCalendarAnimationWrapperView : UIView
+
+@end
---
+++ MBCalendarKit/CalendarKit/Core/CKCalendarAnimationWrapperView.m
@@ -0,0 +1,21 @@
+//
+//  CKCalendarAnimationWrapperView.m
+//  MBCalendarKit
+//
+//  Created by Moshe Berman on 7/31/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+#import "CKCalendarAnimationWrapperView.h"
+
+@implementation CKCalendarAnimationWrapperView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
