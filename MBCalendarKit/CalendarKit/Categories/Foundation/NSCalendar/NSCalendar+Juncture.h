1151ecb0b24396e8131137681539599f99a34716
Moshe Berman <moshefromsinai@gmail.com>
2017-07-31 08:09:28 -0400

    Fixed a misnamed parameter in documentation.

--- MBCalendarKit/CalendarKit/Categories/Foundation/NSCalendar/NSCalendar+Juncture.h
+++ MBCalendarKit/CalendarKit/Categories/Foundation/NSCalendar/NSCalendar+Juncture.h
@@ -18,7 +18,7 @@
 /**
  *  @return The first weekday of a given week as an NSDate.
  *
- *  @param data The date to use to calculate the start of the week.
+ *  @param date The date to use to calculate the start of the week.
  *  @param day An integer value 1-7 representing the weekday. 1 is Sunday, 2 is Monday, etc.
  *  If you pass a value larger than 7, it will probably wrap around, but no guarantees. :D
  *
