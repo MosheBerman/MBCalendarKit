ead791c4c80f5cfdb7b7918f99c2ba88be9dd14d
Moshe Berman <moshefromsinai@gmail.com>
2017-08-08 14:15:21 -0400

    Added a new collection view subclass to replace the old cell display.

---
+++ MBCalendarKit/CalendarKit/Core/CKCalendarGridView.h
@@ -0,0 +1,17 @@
+//
+//  CKCalendarGridView.h
+//  MBCalendarKit
+//
+//  Created by Moshe Berman on 8/8/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+/**
+ The CKCalendarGridView manages the display of cells in the calendar view.
+ */
+@interface CKCalendarGridView : UICollectionView
+
+@end
---
+++ MBCalendarKit/CalendarKit/Core/CKCalendarGridView.m
@@ -0,0 +1,30 @@
+//
+//  CKCalendarGridView.m
+//  MBCalendarKit
+//
+//  Created by Moshe Berman on 8/8/17.
+//  Copyright © 2017 Moshe Berman. All rights reserved.
+//
+
+#import "CKCalendarGridView.h"
+
+@implementation CKCalendarGridView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = UIColor.blueColor;
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
