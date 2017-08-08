1736dc8aa7a4a1f1bf0578a67d46fca982aebfe9
Moshe Berman <moshefromsinai@gmail.com>
2017-08-08 12:43:33 -0400

    Documented CKCalendarEvent.

--- MBCalendarKit/CalendarKit/Core/CKCalendarEvent.h
+++ MBCalendarKit/CalendarKit/Core/CKCalendarEvent.h
@@ -8,14 +8,45 @@
 
 @import UIKit;
 
+
+/**
+ CKCalendarEvent is a model object which represents an event on the calendar. It has a date property, a title, and an optional info dictionary.
+ */
 @interface CKCalendarEvent : NSObject
 
+// MARK: - The Event Date
+
+/**
+ The date when the event occurs.
+ */
 @property (nonatomic, strong) NSDate *date;
-@property (nonatomic, strong) NSString *title;
+
+// MARK: - The Event Title
+
+/**
+ The title for the event.
+ */
+@property (copy) NSString *title;
+
+
+/**
+ A user info dictionary.
+ */
 @property (nonatomic, strong) NSDictionary *info;
+
+// MARK: - Customizing Events with Colors and Images
+
+/**
+ A color to modify the event with.
+ */
 @property (nonatomic, strong) UIColor *color;
 
-@property NSData* image;
+/**
+ An image for to show alongside the event, in the event cell.
+ */
+@property (strong) NSData *image;
+
+// MARK: - Initializers
 
 +(CKCalendarEvent *)eventWithTitle:(NSString *)title andDate:(NSDate *)date andInfo:(NSDictionary *)info;
 +(CKCalendarEvent *)eventWithTitle:(NSString *)title andDate:(NSDate *)date andInfo:(NSDictionary *)info andColor:(UIColor *)color;
