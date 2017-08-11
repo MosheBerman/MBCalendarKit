aa1d8b247690518cd710dd4557e04393b81be750
Moshe Berman <moshefromsinai@gmail.com>
2017-08-10 22:39:10 -0400

    Start moving over the setDate implementation from CKCalendarView.

--- MBCalendarKit/CalendarKit/Core/CKCalendarModel.m
+++ MBCalendarKit/CalendarKit/Core/CKCalendarModel.m
@@ -8,6 +8,7 @@
 
 #import "CKCalendarModel.h"
 #import "NSCalendar+Juncture.h"
+#import "NSCalendarCategories.h"
 
 @interface CKCalendarModel ()
 
@@ -31,6 +32,50 @@
         _date = [NSDate date];
     }
     return self;
+}
+
+// MARK: - Settings the Date
+
+- (void)setDate:(NSDate *)date
+{
+    if (!date) {
+        date = [NSDate date];
+    }
+    
+    NSDateComponents *components = [self.calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:date];
+    date = [self.calendar dateFromComponents:components];
+    
+    BOOL minimumIsBeforeMaximum = [self _minimumDateIsBeforeMaximumDate];
+    
+    if (minimumIsBeforeMaximum) {
+        
+        if ([self _dateIsBeforeMinimumDate:date]) {
+            date = [self minimumDate];
+        }
+        else if([self _dateIsAfterMaximumDate:date])
+        {
+            date = [self maximumDate];
+        }
+    }
+    
+//    if ([[self delegate] respondsToSelector:@selector(calendarView:willSelectDate:)]) {
+//        [[self delegate] calendarView:self willSelectDate:date];
+//    }
+//
+    
+    [self willChangeValueForKey:@"date"];
+    _previousDate = self.date;
+    _date = date;
+    [self didChangeValueForKey:@"date"];
+    
+//    if ([[self delegate] respondsToSelector:@selector(calendarView:didSelectDate:)]) {
+//        [[self delegate] calendarView:self didSelectDate:date];
+//    }
+//    
+//    if ([[self dataSource] respondsToSelector:@selector(calendarView:eventsForDate:)]) {
+//        [self setEvents:[[self dataSource] calendarView:self eventsForDate:date]];
+//        [[self table] reloadData];
+//    }
 }
 
 // MARK: - Getting the First Visible Date
@@ -89,7 +134,59 @@
     }
     
     return lastVisibleDate;
-
 }
+
+// MARK: - Minimum and Maximum Dates
+
+- (BOOL)_minimumDateIsBeforeMaximumDate
+{
+    //  If either isn't set, return YES
+    if (![self _hasNonNilMinimumAndMaximumDates]) {
+        return YES;
+    }
+    
+    return [[self calendar] date:[self minimumDate] isBeforeDate:[self maximumDate]];
+}
+
+- (BOOL)_hasNonNilMinimumAndMaximumDates
+{
+    return [self minimumDate] != nil && [self maximumDate] != nil;
+}
+
+- (BOOL)_dateIsBeforeMinimumDate:(NSDate *)date
+{
+    return [[self calendar] date:date isBeforeDate:[self minimumDate]];
+}
+
+- (BOOL)_dateIsAfterMaximumDate:(NSDate *)date
+{
+    return [[self calendar] date:date isAfterDate:[self maximumDate]];
+}
+
+- (BOOL)_dateIsBetweenMinimumAndMaximumDates:(NSDate *)date
+{
+    //  If there are both the minimum and maximum dates are unset,
+    //  behave as if all dates are in range.
+    if (![self minimumDate] && ![self maximumDate]) {
+        return YES;
+    }
+    
+    //  If there's no minimum, treat all dates that are before
+    //  the maximum as valid
+    else if(![self minimumDate])
+    {
+        return [[self calendar]date:date isBeforeDate:[self maximumDate]];
+    }
+    
+    //  If there's no maximum, treat all dates that are before
+    //  the minimum as valid
+    else if(![self maximumDate])
+    {
+        return [[self calendar] date:date isAfterDate:[self minimumDate]];
+    }
+    
+    return [[self calendar] date:date isAfterDate:[self minimumDate]] && [[self calendar] date:date isBeforeDate:[self maximumDate]];
+}
+
 
 @end
