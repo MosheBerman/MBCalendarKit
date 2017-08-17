5cc92f16c39e3026dd962ceeb53c1a88c4ad0e52
Moshe Berman <moshefromsinai@gmail.com>
2017-08-17 15:39:29 -0400

    Added jump to settings button on iOS 8 in RTL demo.

--- MBCalendarKit/Demo/Demos/Right To Left/RTLDemo.swift
+++ MBCalendarKit/Demo/Demos/Right To Left/RTLDemo.swift
@@ -34,7 +34,14 @@ class RTLDemo: CalendarViewController {
     {
         let title = NSLocalizedString("iOS 8", comment: "")
         let message = NSLocalizedString("This demo requires iOS 9.0 or higher, because it relies on semanticContentAttribute. To see right-to-left support, change your device's language in Settings.app and then come back.", comment: "A note about RTL demo not being supported on iOS 8.")
+        let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        let openSettings = UIAlertAction(title: "Settings", style: .default) { (action: UIAlertAction) in
+            let url = URL(string:UIApplicationOpenSettingsURLString)!
+            UIApplication.shared.openURL(url)
+        }
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(ok)
+        alertController.addAction(openSettings)
         alertController.modalTransitionStyle = .coverVertical
         self.present(alertController, animated: true, completion: nil)
     }
