fdd47e679ea08d8f9682607f953658e8cde2f994
Moshe Berman <mosheberman@users.noreply.github.com>
2017-09-11 13:11:06 -0400

    Comment change.

--- MBCalendarKit/CalendarKit/Core/CKCalendarModel+DaysPerWeek.m
+++ MBCalendarKit/CalendarKit/Core/CKCalendarModel+DaysPerWeek.m
@@ -25,8 +25,7 @@
     //  The "right" way to do this is to call
     //  `[self.calendar rangeOfUnit:NSCalendarUnitWeekday inUnit:NSCalendarUnitWeekOfYear forDate:self.date].length`
     //
-    // No calendar supported by iOS has a non-7-day week,
-    // so this is safe to do.
+    // No calendar supported by iOS has a non-7-day week, so this is safe to do.
     
     return 7;
 }
