b312f1f8e7fd3599c38b82017ba8bfeb40eaed09
MosheBerman <mosheberman@users.noreply.github.com>
2019-03-19 10:00:13 -0400

    Add tvOS demo target.

--- Demo/iOS/MBCalendarKit-Bridging-Header.h
+++ Demo/iOS/MBCalendarKit-Bridging-Header.h
@@ -2,7 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
-#import "MBCalendarKit.h"
 #import "CKDemoViewController.h"
 #import "NSCalendarCategories.h"
---
+++ Demo/tvOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Moshe Berman on 3/18/19.
+//  Copyright © 2019 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
